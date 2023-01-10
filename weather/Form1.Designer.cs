b2adfe7869dba7c138f2fe394306d416d9cfe8bf
HeiLohv <hlohvansuu@student.vaasa.fi>
2023-01-10 15:37:35 +0200

    Början av projektet

---
+++ weather/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.labelLocationSearch = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelWeather = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.pictureBoxIcon1 = new System.Windows.Forms.PictureBox();
+            this.labelLocation = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemperature.Location = new System.Drawing.Point(274, 210);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(93, 33);
+            this.labelTemperature.TabIndex = 0;
+            this.labelTemperature.Text = "label1";
+            // 
+            // labelLocationSearch
+            // 
+            this.labelLocationSearch.AutoSize = true;
+            this.labelLocationSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLocationSearch.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelLocationSearch.Location = new System.Drawing.Point(151, 143);
+            this.labelLocationSearch.Name = "labelLocationSearch";
+            this.labelLocationSearch.Size = new System.Drawing.Size(74, 20);
+            this.labelLocationSearch.TabIndex = 1;
+            this.labelLocationSearch.Text = "Location:";
+            this.labelLocationSearch.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(225, 143);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(151, 20);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // labelWeather
+            // 
+            this.labelWeather.AutoSize = true;
+            this.labelWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWeather.Location = new System.Drawing.Point(275, 248);
+            this.labelWeather.Name = "labelWeather";
+            this.labelWeather.Size = new System.Drawing.Size(60, 24);
+            this.labelWeather.TabIndex = 3;
+            this.labelWeather.Text = "label3";
+            this.labelWeather.Click += new System.EventHandler(this.labelWeather_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(408, 65);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(51, 20);
+            this.labelDate.TabIndex = 4;
+            this.labelDate.Text = "label1";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(408, 89);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(51, 20);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "label1";
+            // 
+            // pictureBoxIcon1
+            // 
+            this.pictureBoxIcon1.Location = new System.Drawing.Point(153, 210);
+            this.pictureBoxIcon1.Name = "pictureBoxIcon1";
+            this.pictureBoxIcon1.Size = new System.Drawing.Size(99, 87);
+            this.pictureBoxIcon1.TabIndex = 6;
+            this.pictureBoxIcon1.TabStop = false;
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLocation.Location = new System.Drawing.Point(275, 273);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(60, 24);
+            this.labelLocation.TabIndex = 7;
+            this.labelLocation.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.pictureBoxIcon1);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelWeather);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.labelLocationSearch);
+            this.Controls.Add(this.labelTemperature);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.Label labelLocationSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label labelWeather;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.PictureBox pictureBoxIcon1;
+        private System.Windows.Forms.Label labelLocation;
+    }
+}
+
